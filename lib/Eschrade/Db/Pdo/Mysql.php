78f3a02845aa9c4a3940cd38806dbb6407bd7803
Charley Wu <akunzai@gmail.com>
2020-10-08 13:25:59 +0800

    Code cleanup

--- lib/Eschrade/Db/Pdo/Mysql.php
+++ lib/Eschrade/Db/Pdo/Mysql.php
@@ -8,20 +8,19 @@ class Eschrade_Db_Pdo_Mysql extends Magento_Db_Adapter_Pdo_Mysql
         if ($config instanceof Zend_Config) {
             $config = $config->toArray();
         }
-        if (isset($config['secure_driver_options']) && is_array($config['secure_driver_options'])) {
-            if (!isset($config['driver_options']) || !is_array($config['driver_options'])) {
-                $config['driver_options'] = [];
-            }
-            foreach ($config['secure_driver_options'] as $key => $value) {
-                $constant = 'PDO::' . $key;
-                $constVal = constant($constant);
-                if ($constVal !== null) {
-                    $config['driver_options'][$constVal] = $value;
+        if (isset($config['secure_driver_options'])) {
+            if (is_array($config['secure_driver_options'])){
+                if (!isset($config['driver_options']) || !is_array($config['driver_options'])) {
+                    $config['driver_options'] = [];
+                }
+                foreach ($config['secure_driver_options'] as $key => $value) {
+                    $constant = 'PDO::' . $key;
+                    $constVal = constant($constant);
+                    if ($constVal !== null) {
+                        $config['driver_options'][$constVal] = $value;
+                    }
                 }
             }
-        }
-        if (isset($config['secure_driver_options']))
-        {
             unset($config['secure_driver_options']);
         }
         parent::__construct($config);
